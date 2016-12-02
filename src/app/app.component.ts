4aa1d503eabfbb7c92138e2123946adbcbc263a6
mohankumaranna <mohankumar_anna@hotmail.com>
2016-12-02 12:43:20 +0530

    setting up configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1> <p>Apt Maint Acct Tracking App</p>`,
 })
 export class AppComponent  { name = 'Angular'; }
