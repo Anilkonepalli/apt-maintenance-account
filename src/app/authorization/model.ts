e1a3414c5d772bd54567daf74d6b15073491af2c
mohankumaranna <mohankumar_anna@hotmail.com>
2017-02-10 10:54:09 +0530

    removed dead codes

--- src/app/authorization/model.ts
+++ src/app/authorization/model.ts
@@ -16,75 +16,15 @@ export class Authorization {
 	}
 
 	public allowsView(owner: number): boolean {
-
 		return this.allows('R', owner);
-
-/*		let permissions = this.permissions.filter(perm => { // find permissions with Read grants
-			return perm.operations.indexOf('R') >= 0;
-		});
-
-		if(permissions.length < 1) return false; // no permissions found
-
-		permissions = permissions.filter(perm => { // find permissions with condition
-			return perm.condition != null && perm.condition != '';
-		});
-
-		if(permissions.length < 1) return true; // permission(s) exist but has no condition
-
-		// evaluate condition
-		let fn = new Function("data", permissions[0].condition);
-		let data = { userId: this.user, ownerId: owner };
-
-		return fn(data); */
-
 	}
 
-
 	public allowsEdit(owner: number): boolean {
-
 		return this.allows('U', owner); 
-
-/*		let permissions = this.permissions.filter(perm => { // find permissions with Read grants
-			return perm.operations.indexOf('U') >= 0;
-		});
-
-		if(permissions.length < 1) return false; // no permissions found
-
-		permissions = permissions.filter(perm => { // find permissions with condition
-			return perm.condition != null && perm.condition != '';
-		});
-
-		if(permissions.length < 1) return true; // permission(s) exist but has no condition
-
-		// evaluate condition
-		let fn = new Function("data", permissions[0].condition);
-		let data = { userId: this.user, ownerId: owner };
-
-		return fn(data); */
-
 	}
 
 	public allowsDelete(owner: number): boolean {
 		return this.allows('D', owner);
-
-/*		let permissions = this.permissions.filter(perm => { // find permissions with Read grants
-			return perm.operations.indexOf('D') >= 0;
-		});
-
-		if(permissions.length < 1) return false; // no permissions found
-
-		permissions = permissions.filter(perm => { // find permissions with condition
-			return perm.condition != null && perm.condition != '';
-		});
-
-		if(permissions.length < 1) return true; // permission(s) exist but has no condition
-
-		// evaluate condition
-		let fn = new Function("data", permissions[0].condition);
-		let data = { userId: this.user, ownerId: owner };
-
-		return fn(data); */
-
 	}
 
 	private allows(action: string, owner:number): boolean {
@@ -107,4 +47,5 @@ export class Authorization {
 
 		return fn(data);
 	}
+
 }
