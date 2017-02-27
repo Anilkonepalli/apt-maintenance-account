a3a9f738e51338eb2886c77fe2e5df51e76e4347
mohankumaranna <mohankumar_anna@hotmail.com>
2017-02-27 14:40:31 +0530

    writing test specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,5 @@
+/*
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }                        from '@angular/platform-browser';
 import { DebugElement }              from '@angular/core';
@@ -35,40 +37,51 @@ describe('AppComponent (inline template)', () => {
   });
 });
 
-/*
-import { AppComponent }                     from './app.component';
+*/
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }                               from '@angular/platform-browser';
 import { DebugElement }                     from '@angular/core';
 
+import { AppComponent }                     from './app.component';
+
 describe('AppComponent', function () {
 
-  let de: DebugElement;
   let comp: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement; 
+  let el: HTMLElement;
 
+  // async beforeEach
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      declarations: [ AppComponent ] // declare the test component
     })
-    .compileComponents();
+    .compileComponents(); // compile template and css files
   }));
 
+  // synchronous beforeEach
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
+    comp = fixture.componentInstance; // AppComponent test instance
+
+    // query for the title <h1> by CSS element selector
     de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
   });
 
-  it('should create component', () => expect(comp).toBeDefined() );
+  it('should display original title', () => {
+    fixture.detectChanges();
+    expect(el.textContent).toContain(comp.title);
+  });
+
+/*  it('should create component', () => expect(comp).toBeDefined() );
 
   it('should have expected <h1> text', () => {
     fixture.detectChanges();
     const h1 = de.nativeElement;
     expect(h1.innerText).toMatch(/angular/i,
       '<h1> should say something about "Angular"');
-  });
+  }); */
 
 });
-*/
