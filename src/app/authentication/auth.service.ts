3d7953d4c559c904c84f59a10c406e492e4fe89f
mohankumaranna <mohankumar_anna@hotmail.com>
2017-03-19 20:07:33 +0530

    adding social logins:

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -20,6 +20,7 @@ contentHeaders.append('Content-Type', 'application/json');
 export class AuthService {
 
     isLoggedIn: boolean = false;
+    isSocialLoggedIn: boolean = false;
     JwtHelper = new JwtHelper();
     message: Message;
 
@@ -60,5 +61,39 @@ export class AuthService {
     logout(): void {
         this.logger.info('Logged Out @auth.service...');
         this.isLoggedIn = false;
+    }
+
+    socialLogin(network: String, socialToken: String): Observable<object> {
+        let data = JSON.stringify({ network: network, socialToken: socialToken });
+        let url = process.env.API_URL + '/api/socialLogin';
+
+        this.http.post(url, data, { headers: contentHeaders }).subscribe(
+            response => {
+                this.logger.info('Logged In successfully...');
+                let res = response.json();
+                localStorage.setItem('id_token', res.id_token);
+                let decodedJwt = res.id_token && this.JwtHelper.decodeToken(res.id_token);
+                localStorage.setItem('userId', decodedJwt.id);
+                this.isSocialLoggedIn = true;
+                this.message = new InfoMessage("Success", "log...");
+            },
+            error => {
+                this.isSocialLoggedIn = false;
+
+                // ToDo: Use a remote logging infrastructure later
+                let errMsg: string;
+                if (error instanceof Response) {
+                    errMsg = `${error.status} - ${error.statusText || ''}`;
+                } else {
+                    errMsg = error.message ? error.message : error.toString();
+                }
+                this.message = new ErrorMessage("Failure", errMsg);
+            });
+        return Observable.of(this).delay(1000);
+    }
+
+    socialLogout(): void {
+        this.logger.info('Logged Out @auth.service...');
+        this.isSocialLoggedIn = false;
     }
 }
--- src/app/authentication/social/login.component.ts
+++ src/app/authentication/social/login.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+
+import { AuthService }        from '../auth.service';
+import { Message }            from '../../shared';
+
+
+declare const hello: any;
 
 @Component({
     selector: 'social-login',
@@ -6,14 +13,59 @@ import { Component } from '@angular/core';
 })
 
 export class SocialLoginComponent {
+    email: String;
+    socialToken: String;
+    serverToken: String;
+    message: Message = new Message();
 
-    constructor() {
-        console.log('constructor() called...');
+    constructor(public authService: AuthService, public router: Router) {
+        //console.log('constructor() called @SocialLoginComponent...');
         hello.init({
             facebook: '988055257994411',
-            google: '<Google client id>'
-        });
+            google: '<Google client id>' // yet to add actual id
+        }, { scope: 'public_profile, email' });
+
+        hello.on('auth.login', this.authenticate);
     }
 
+    authenticate(auth: any) {
+        //console.log(' social authenticate(auth) called...');
+        //console.log('auth is...'); console.log(auth);
+        // Save the social token
+        this.socialToken = auth.authResponse.access_token;
+
+        /*      // sample api calls of hello js api("me") gets profile details
+                hello(auth.network).api("me")
+                    .then((res: any) => {
+                        console.log('Response object...'); console.log(res);
+                    })
+                    .catch((err: any) => {
+                        console.log('Error in hello api call to me ' + err);
+                    });
+        */
+
+        // Auth with our app server using the social token
+        this.authService.socialLogin(auth.network, this.socialToken).subscribe(() => {
+
+            if (this.authService.isSocialLoggedIn) {
+
+                // Get the redirect URL from our auth service
+                // If no redirect is set, use the default
+                let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/home';
+
+                // Set our navigation extras object
+                // that passes on our global query params and fragment
+                let navigationExtras: NavigationExtras = {
+                    preserveQueryParams: true,
+                    preserveFragment: true
+                };
+
+                // Redirect the user
+                this.router.navigate([redirect], navigationExtras);
+            } else { // login failed
+                this.message = this.authService.message;
+            }
+        });
+    }
 
 }
