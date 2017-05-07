1ccbec035a1f781712521502875a12ad6e7d18d0
mohankumaranna <mohankumar_anna@hotmail.com>
2017-05-07 23:40:07 +0530

    removed guard on home page

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,24 +1,32 @@
-import { Component, Host } from '@angular/core';
+import { Component, Host, OnInit } from '@angular/core';
 import { JwtHelper } 		   from 'angular2-jwt';
 
 import { AppComponent } 	 from './app.component';
+import { Logger }		   from './logger/default-log.service';
+import { AuthService } from './authentication/auth.service';
 
 @Component({
-    selector: 'home',
-    template: `
+  selector: 'home',
+  template: `
 		<h1>Welcome to Apt Maint Acct</h1>
-		<h2 *ngIf="jwt">Your JWT is:</h2>
-		<pre *ngIf="jwt" class="jwt"><code>{{ jwt }}</code></pre>
-		<pre *ngIf="jwt" class="jwt"><code>{{ decodedJwt | json }}</code></pre>
+    <h2 *ngIf="authService.isLoggedIn">Your JWT is:</h2>
+		<pre *ngIf="authService.isLoggedIn" class="jwt"><code>{{ jwt }}</code></pre>
+		<pre *ngIf="authService.isLoggedIn" class="jwt"><code>{{ decodedJwt | json }}</code></pre>
 	`
 })
-export class HomeComponent {
-    jwt: string;
-    decodedJwt: string;
-    JwtHelper = new JwtHelper();
+export class HomeComponent implements OnInit {
+  jwt: string;
+  decodedJwt: string;
+  JwtHelper = new JwtHelper();
 
-    constructor( @Host() parent: AppComponent) {
-        this.jwt = localStorage.getItem('id_token');
-        this.decodedJwt = this.jwt && this.JwtHelper.decodeToken(this.jwt);
-    }
+  constructor(
+    @Host() parent: AppComponent,
+    public authService: AuthService,
+    public logger: Logger
+  ) { }
+
+  ngOnInit(): void {
+    this.jwt = localStorage.getItem('id_token');
+    this.decodedJwt = this.jwt && this.JwtHelper.decodeToken(this.jwt);
+  }
 }
