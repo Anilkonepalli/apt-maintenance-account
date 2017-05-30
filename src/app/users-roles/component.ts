78bb0eb696d4b0fafe6c22c6d02aa04dd63471f8
mohankumaranna <mohankumar_anna@hotmail.com>
2017-05-30 21:47:42 +0530

    improved UI on users-roles

--- src/app/users-roles/component.ts
+++ src/app/users-roles/component.ts
@@ -8,6 +8,7 @@ import { User }														from '../users/model';
 import { UserRoleService }								from './service';
 
 import 'rxjs/add/operator/switchMap';
+import * as _                             from 'lodash';
 
 var list_css = require('./component.css');
 var list_css_string = list_css.toString();
@@ -15,96 +16,101 @@ var list_html = require('./component.html');
 var list_html_string = list_html.toString();
 
 @Component({
-    selector: 'user-role',
-    styles: [list_css_string],
-    templateUrl: list_html_string
+  selector: 'user-role',
+  styles: [list_css_string],
+  templateUrl: list_html_string
 })
 export class UserRoleComponent implements OnInit {
-    //----------------------------------------------------------------------------------
-    //   Users (lstream)   |       Roles (rstream = attached + detached)               |
-    //                     |   AttachedStream   |  DetachedStream (or Available List)) |
-    //----------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------------
+  //   Users (lstream)   |       Roles (rstream = attached + detached)               |
+  //                     |   AttachedStream   |  DetachedStream (or Available List)) |
+  //----------------------------------------------------------------------------------
 
-    lstream: Observable<User[]>;  // stream on left side models
-    attachedStream: Observable<Role[]>;
-    detachedStream: Observable<Role[]>;
-    rstream: Observable<Role[]>; // rstream holds right side models (attached + detached)
+  lstream: Observable<User[]>;  // stream on left side models
+  attachedStream: Observable<Role[]>;
+  detachedStream: Observable<Role[]>;
+  rstream: Observable<Role[]>; // rstream holds right side models (attached + detached)
 
-    lId: number;       // selected left model
-    aIds: Array<number>;  // ids of selected attached models
-    dIds: Array<number>;  // ids of selected detached models
+  lId: number;       // selected left model
+  aIds: Array<number>;  // ids of selected attached models
+  dIds: Array<number>;  // ids of selected detached models
 
-    constructor(
-        private service: UserRoleService,
-        private route: ActivatedRoute,
-        private router: Router
-    ) { }
+  constructor(
+    private service: UserRoleService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
-    ngOnInit(): void {
-        this.lstream = this.route.params
-            .switchMap((params: Params) => {
-                return this.service.getlmodels();
-            });
+  ngOnInit(): void {
+    this.lstream = this.route.params
+      .switchMap((params: Params) => {
+        return this.service.getlmodels();
+      })
+      .map((users: User[]) => {
+        return _.sortBy(users, [function(obj: User) { return obj.name; }]);
+      });
 
-        this.rstream = this.route.params
-            .switchMap((params: Params) => {
-                return this.service.getrmodels();
-            });
+    this.rstream = this.route.params
+      .switchMap((params: Params) => {
+        return this.service.getrmodels();
+      });
 
-        this.detachedStream = this.rstream; // initially rstream are in detachedStream
-    }
+    this.detachedStream = this.rstream; // initially rstream are in detachedStream
+  }
 
-    onlSelect(model: User): void {
-        this.service.getAttachedModels(model.id)
-            .then(amodels => {
-                this.attachedStream = Observable.of(amodels);
-                let attachedaIds = amodels.map(amodel => amodel.id);
-                this.updateDetachedModelsExcluding(attachedaIds);
-                return null;
-            });
-    }
+  onlSelect(model: User): void {
+    this.service.getAttachedModels(model.id)
+      .then(amodels => {
+        this.attachedStream = Observable.of(amodels);
+        let attachedaIds = amodels.map(amodel => amodel.id);
+        this.updateDetachedModelsExcluding(attachedaIds);
+        return null;
+      });
+  }
 
-    attach() {
-        this.detachedStream.subscribe(dmodel => {
-            let dIdNums = this.dIds.map(each => +each); // convert string id into integer
-            let dmodels = dmodel.filter(each => !dIdNums.includes(each.id));
-            this.detachedStream = Observable.of(dmodels);
+  attach() {
+    this.detachedStream.subscribe(dmodel => {
+      let dIdNums = this.dIds.map(each => +each); // convert string id into integer
+      let dmodels = dmodel.filter(each => !dIdNums.includes(each.id));
+      this.detachedStream = Observable.of(dmodels);
 
-            let dIdsNew = dmodels.map(dmodel => dmodel.id); // collect aIds of new amodels
-            this.updateAttachedModelsExcluding(dIdsNew);
-        });
-    }
+      let dIdsNew = dmodels.map(dmodel => dmodel.id); // collect aIds of new amodels
+      this.updateAttachedModelsExcluding(dIdsNew);
+    });
+  }
 
-    detach() {
-        this.attachedStream.subscribe(amodel => {   // remove the selected items from attached models
-            let aIdsNums = this.aIds.map(each => +each); // convert string id into integer
-            let amodels = amodel.filter(each => !aIdsNums.includes(each.id)); // filter out selected amodels
-            this.attachedStream = Observable.of(amodels); // update attached stream after filtering
+  detach() {
+    this.attachedStream.subscribe(amodel => {   // remove the selected items from attached models
+      let aIdsNums = this.aIds.map(each => +each); // convert string id into integer
+      let amodels = amodel.filter(each => !aIdsNums.includes(each.id)); // filter out selected amodels
+      this.attachedStream = Observable.of(amodels); // update attached stream after filtering
 
-            let aIdsNew = amodels.map(amodel => amodel.id); // collect aIds of new amodels
-            this.updateDetachedModelsExcluding(aIdsNew); // now, dmodels = rmodels - amodels
-        });
-    }
+      let aIdsNew = amodels.map(amodel => amodel.id); // collect aIds of new amodels
+      this.updateDetachedModelsExcluding(aIdsNew); // now, dmodels = rmodels - amodels
+    });
+  }
 
-    save() {
-        this.attachedStream.subscribe(amodel => {
-            let aIds = amodel.map(each => each.id);
-            this.service.saveAttachedModels(this.lId, aIds);
-        })
-    }
+  save() {
+    this.attachedStream.subscribe(amodel => {
+      let aIds = amodel.map(each => each.id);
+      this.service.saveAttachedModels(this.lId, aIds);
+    })
+  }
 
-    private updateDetachedModelsExcluding(attachedaIds: number[]) {
-        this.rstream.subscribe(rmodel => {
-            let availablerModels = rmodel.filter(each => !attachedaIds.includes(each.id));
-            this.detachedStream = Observable.of(availablerModels);
-        });
-    }
+  private updateDetachedModelsExcluding(attachedaIds: number[]) {
+    this.rstream.subscribe(rmodel => {
+      let availablerModels = rmodel.filter(each => !attachedaIds.includes(each.id));
+      this.detachedStream = Observable.of(availablerModels);
+      this.save();
+    });
+  }
 
-    private updateAttachedModelsExcluding(attacheddIds: number[]) {
-        this.rstream.subscribe(rmodel => {
-            let availablerModels = rmodel.filter(each => !attacheddIds.includes(each.id));
-            this.attachedStream = Observable.of(availablerModels);
-        });
-    }
+  private updateAttachedModelsExcluding(attacheddIds: number[]) {
+    this.rstream.subscribe(rmodel => {
+      let availablerModels = rmodel.filter(each => !attacheddIds.includes(each.id));
+      this.attachedStream = Observable.of(availablerModels);
+      this.save();
+    });
+  }
 
 }
